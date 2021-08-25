825d489f2711feb01c089d17465519ce0e764d70
reedwane <reedart07@gmail.com>
2021-08-25 13:10:38 +0100

    src attribute edited for the images

--- blogr/index.js
+++ blogr/index.js
@@ -18,10 +18,10 @@ for (acc of accordion) {
 
 bars.addEventListener("click", function () {
 	let icon = bars.getAttribute("src");
-	if (icon == "/images/icon-hamburger.svg") {
-		bars.setAttribute("src", "/images/icon-close.svg");
+	if (icon == "images/icon-hamburger.svg") {
+		bars.setAttribute("src", "images/icon-close.svg");
 	} else {
-		bars.setAttribute("src", "/images/icon-hamburger.svg");
+		bars.setAttribute("src", "images/icon-hamburger.svg");
 	}
 	nav.classList.toggle("nav-open");
 
