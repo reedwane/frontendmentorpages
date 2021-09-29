96dc3c32284afed371f831825ac7798099975230
reedwane <reedart07@gmail.com>
2021-09-29 11:51:15 +0100

    comments added to the js file for the tip-calculator app

--- tip-calculator-app-main/app.js
+++ tip-calculator-app-main/app.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", (e) => {
+	//declaring the variable needed in the app
+	//
 	var bills = document.querySelector(".bills input");
 	var bill = 142.55;
 	var tipBox = document.querySelector(".tips");
@@ -7,7 +9,6 @@ window.addEventListener("DOMContentLoaded", (e) => {
 	var percent = 0.15;
 	var custom = document.querySelector(".custom input");
 	var cusPercent = custom.value;
-	//var defaultColor = 'green';
 	var defaultPercent = document.querySelector("#default");
 	var noOfPeople = document.querySelector(".people input");
 	var people = document.querySelector(".people input").value;
@@ -19,14 +20,19 @@ window.addEventListener("DOMContentLoaded", (e) => {
 	var tipAmount; //per person
 	var totalAmount; //per person
 
+	//
+	//the function for calculating the tip each person is giving from the total bill
 	const calcTipAmount = (bill, percent, people) => {
 		return (bill / people) * percent;
 	};
-
+	//
+	//the function for calculating the total amount each person will pay
 	const calcTotalAmount = (bill, tipAmount, people) => {
 		return bill / people + tipAmount;
 	};
 
+	//
+	//the function for recalculating the amounts when new inputs are made
 	const update = (people, tipAmount, totalAmount, bill, percent) => {
 		tipAmount = calcTipAmount(bill, percent, people);
 		totalAmount = calcTotalAmount(bill, tipAmount, people);
@@ -41,12 +47,18 @@ window.addEventListener("DOMContentLoaded", (e) => {
 		reset.style.opacity = 2;
 	};
 
+	//
+	//reacting to change in the total bill input
+
 	bills.addEventListener("input", (e) => {
 		people = 1;
 		noOfPeople.value = 1;
 		bill = e.target.value;
 		update(people, tipAmount, totalAmount, bill, percent);
 	});
+
+	//
+	//reacting to change in the percentage of tip selected
 
 	tipBox.addEventListener("click", (e) => {
 		defaultPercent.removeAttribute("id");
@@ -68,11 +80,17 @@ window.addEventListener("DOMContentLoaded", (e) => {
 		update(people, tipAmount, totalAmount, bill, percent);
 	});
 
+	//
+	//reacting to selection of a custom percentage for the tip
+
 	custom.addEventListener("input", (e) => {
 		cusPercent = e.target.value / 100;
 		percent = cusPercent;
 		update(people, tipAmount, totalAmount, bill, percent);
 	});
+
+	//
+	//changing the amounts for each people based on the total number of people paying the bill
 
 	noOfPeople.addEventListener("input", (e) => {
 		people = e.target.value;
@@ -86,6 +104,9 @@ window.addEventListener("DOMContentLoaded", (e) => {
 		}
 	});
 
+	//
+	// clearing all input and reseting the values with the reset button
+
 	reset.addEventListener("click", (e) => {
 		e.preventDefault();
 		e.target.style.opacity = 0.3;
@@ -98,6 +119,9 @@ window.addEventListener("DOMContentLoaded", (e) => {
 		document.querySelector("#tip-amount").innerText = `$0.00`;
 		document.querySelector("#total-amount").innerText = `$0.00`;
 	});
+
+	//
+	//the default result from the default values set
 
 	update(people, tipAmount, totalAmount, bill, percent);
 });
